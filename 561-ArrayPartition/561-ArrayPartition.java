4a336dc3847efc0756736ad3b512ac78e5a29df4
Vibindev16 <vibindev200516@gmail.com>
2026-02-26 09:56:16 +0530

    Create 561-ArrayPartition

---
+++ 561-ArrayPartition/561-ArrayPartition.java
@@ -0,0 +1,13 @@
+// Last updated: 2/26/2026, 9:56:16 AM
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int maxSum = 0;
+
+        for(int i = 0; i<nums.length; i=i+2)
+        {
+            maxSum += nums[i];
+        }
+        return maxSum;
+    }
+}
